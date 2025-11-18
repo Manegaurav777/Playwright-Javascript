65bc936d6cb973da096039c574ad18cf41d48964
gaurav <manegaurav310@gmail.com>
2025-11-18 11:37:31 +0530

    file update

--- tests/VerifyErrorMsg.spec.js
+++ tests/VerifyErrorMsg.spec.js
@@ -6,10 +6,10 @@ test("Verify error message for invalid login",async function({page}){
     console.log(await page.viewportSize().width)
     console.log(await page.viewportSize().height)
     await page.viewportSize({width:1600,height:900})
-   /* await page.getByPlaceholder('Username').fill('Admin')
+    await page.getByPlaceholder('Username').fill('Admin')
     await page.getByPlaceholder('Password').fill('admin1234') //intentionally wrong password
     await page.locator("//button[@type='submit']").click()
     const errormessage=  await page.locator("//p[contains(@class,'-alert-content-text')]").textContent()
     console.log("Error message is :"+errormessage)
-    expect(errormessage.includes("Invalid")).toBeTruthy()*/
+    expect(errormessage.includes("Invalid")).toBeTruthy()
 })
